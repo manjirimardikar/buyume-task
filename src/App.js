b6ff1782fe48a7adb94fb4696427ef8028912911
manjirimardikar <manjari.mardikar28@gmail.com>
2023-08-10 18:14:32 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-
+import React, { useState } from 'react';
 import './App.css';
-import React,{useState} from 'react';
 
 function App() {
-
-  const [searchText,setSearchText] = useState('');
-  const[filteredPosts, setFilteredPosts] = useState([]);
-
+  const [searchText, setSearchText] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  
   const handleSearchChange = (event) => {
     setSearchText(event.target.value);
   };
-
-  const handleSearchClick = async() => {
-    try{
+  
+  const handleSearchClick = async () => {
+    try {
       const response = await fetch('https://jsonplaceholder.typicode.com/posts');
       const data = await response.json();
       const filtered = data.filter(post => post.title.includes(searchText));
       setFilteredPosts(filtered);
-    } catch (error){
+    } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
+  
   return (
     <div className="App">
-      <header>
-       <input type="text" value={searchText} onChange={handleSearchChange}/>
-       <button onClick={handleSearchClick}>Search Here</button>
-       <ul>
-         {filteredPosts.map(post => (
-           <li key={post.id}>{post.title}</li>
-         ))}
-         </ul>
-       </header>
+      <header className="App-header">
+        <input type="text" value={searchText} onChange={handleSearchChange} />
+        <button onClick={handleSearchClick}>Search</button>
+        <div className="card-container">
+          {filteredPosts.map(post => (
+            <div key={post.id} className="card">
+              {post.title}
+            </div>
+          ))}
+        </div>
+      </header>
     </div>
   );
 }
 
 export default App;
+
